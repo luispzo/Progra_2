dc269d089cb4961e7d2bf9a6a80d5f336ebd632b
LuisPozo <luis.pozopozo31@gmail.com>
2024-10-14 09:29:37 -0500

    first commit

---
+++ src/Principal2.java
@@ -0,0 +1,21 @@
+public class Principal2 {
+    public static void main(String[] args) {
+        Automovil2  bmw = new Automovil2("BMW");
+
+        //bmw.setMarca("BMW");
+        bmw.setModelo("i22");
+        bmw.setColor("Negro");
+        bmw.setCilindraje("2");
+
+        System.out.println("bmw = " + bmw.verDetalle());
+        //cada objeto aunque tenga las mismas caracteristicas, son diferentes
+
+        //bmw.equals(bmw2) para compracacion
+        Automovil2 bmw2 = new Automovil2("Mercedes","ML200");
+        //bmw2.setMarca("BMW");
+        //bmw2.setModelo("i22");
+        bmw2.setColor("Negro");
+        bmw2.setCilindraje("2");
+        System.out.println("bmw = " + bmw2.verDetalle());
+    }
+}
