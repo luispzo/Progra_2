ecc073836ef60d0d51a26d497b86570b115d8512
luispzo <luis.pozopozo31@gmail.com>
2024-11-05 09:03:42 -0500

    first commit

--- Clase_Arreglos/src/ListaArreglos.java
+++ Clase_Arreglos/src/ListaArreglos.java
@@ -8,10 +8,14 @@ public class ListaArreglos {
         List<String> listaColores = new ArrayList<>();
 
         //Creacion medante la clase Arrays
+        //aqui es metiendo un vector a una lista
         String[] nombres = {"Arturo","Daniel","Pamela"}; //creo un arreglo y luego le paso a una lista
         List<String> listaNombres = Array.asList(nombres);
 
         //Adicion de elementos individuales
+        //metodo add añade manualmente
+        //hay un metodo llamado addall
+        //hay un metodo llamado remove
         listaColores.add("Verde");
         listaColores.add("Amarillo");
 
